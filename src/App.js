c7d7ebd63a4b0a3129d2efc1f9abd4d256ba465e
Hao Sitong <hst1142007403@gmail.com>
2022-06-25 02:46:57 +0800

    1.0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { Socket } from 'phoenix';
-import { useEffect, useState, useReducer } from 'react';
+import { useEffect, useState, useReducer, useRef } from 'react';
 import axios from 'axios';
 
 function append(state, action) {
-  return [...state, action];
-}
-
-function playerReducer(state, action) {
-  switch (action) {
-    case "+":
-      return state + 1;
-    case "-":
-      return state - 1;
-    default:
-      return
+  if (action === "#reset") {
+    return [];
   }
+  return [...state, action];
 }
 
 function App() {
@@ -28,9 +20,16 @@ function App() {
   const [guess, setGuess] = useState("");
   const [name, setName] = useState("");
   const [messages, updateMessages] = useReducer(append, []);
-  const [players, updatePlayers] = useReducer(playerReducer, 0);
+  const [players, setPlayers] = useState([]);
   const [roomCreated, setRoomCreated] = useState(false);
   const [isReady, setIsReady] = useState(false);
+  const [roundStarted, setRoundStarted] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
+  const [guessEnabled, setGuessEnabled] = useState(false);
+
+  const stateRef = useRef();
+  stateRef.current = players;
 
   useEffect(() => { //http request
     console.log("useEffect, []")
@@ -79,7 +78,7 @@ function App() {
   useEffect(() => { //send ready after ready
     if (!isReady) return;
     channel.push("ready")
-  }, [isReady, channel])
+  }, [isReady])
 
   function handleJoin(event) {
     console.log("handleJoin: " + roomId)
@@ -90,14 +89,52 @@ function App() {
 
     if (event !== null) event.preventDefault();
 
-    const channel = socket.channel("room:" + roomId, {"playerName": name});
+    const channel = socket.channel("room:" + roomId, { "playerName": name });
 
     channel.on("joined", msg => {
       updateMessages(msg.playerName + " joined.")
-      updatePlayers("+");
+      setPlayers(msg.data.players.map(x => [x.playerId, x.playerName]));
     });
-    channel.on("new_guess", msg => updateMessages(msg.playerName + " made a guess: " + msg.guess));
-    channel.on("ready", msg => updateMessages(msg.playerName + " is ready."));
+    channel.on("new_guess", msg =>
+      updateMessages(msg.playerName + " made a guess, row: " + msg.row)
+    );
+    channel.on("ready", msg => updateMessages(msg.playerName + " is ready.")
+    );
+    channel.on("start_game", msg => setGameStarted(true));
+    channel.on("start_round", msg => {
+      updateMessages("#reset");
+      updateMessages("New round started.");
+      setRoundStarted(true);
+      setGameStarted(true);
+      setGuessEnabled(true);
+    }
+    );
+    channel.on("end_round", msg => {
+      if (msg.gameOver) {
+        setGameOver(true);
+        setTimeout(() => {
+          setRoomId("");
+          setGuess("");
+          setName("");
+          setPlayers([]);
+          setRoomCreated(false);
+          setIsReady(false);
+          setRoundStarted(false);
+          setGameStarted(false);
+          updateMessages("#reset");
+          setChannel(null);
+          setGameOver(false);
+        }, 10000)
+      }
+      updateMessages("Round ended. The word was " + msg.word);
+      for (let key in msg.scores) {
+        stateRef.current.filter(x => x[0] === key).forEach(s =>
+          updateMessages(s[1] + " score: " + msg.scores[key]));
+      }
+      setRoundStarted(false);
+      setGuessEnabled(false);
+    }
+    )
     channel.on("presence_diff", msg => {
       // Object.entries(msg.joins).forEach(([id, data]) => {
       //   updateMessages(id + " joined again.");
@@ -105,7 +142,7 @@ function App() {
       Object.entries(msg.leaves).forEach(([id, data]) => {
         updateMessages(id + " left.");
       });
-  })
+    })
 
     channel.join()
       .receive('ok', () => {
@@ -131,7 +168,11 @@ function App() {
     channel.push("new_guess", { "guess": guess })
       .receive('ok', (reply) => {
         console.log("guess result " + reply.result)
-        updateMessages("guess evaluated: " + reply.result)
+        updateMessages("Guess evaluated: " + reply.result)
+        if (reply.row === 6) {
+          updateMessages("You ran out of guesses :(")
+          setGuessEnabled(false);
+        }
       });
     event.target.reset();
   }
@@ -140,17 +181,17 @@ function App() {
   return (
     playerId === "" || (
       <div className="App">
-        <img src={logo} className="App-logo" alt="logo" style={{ "width": "320px", "marginBottom": "0px" }} />
+        <img src={logo} className="App-logo" alt="logo" style={{ "width": "320px", "marginBottom": "-20px" }} />
         {channel == null ?
           (<div>
             <h2 style={{ "marginTop": "0px" }}>
-              Mordle Demo 0.1 (Alpha)
+              Mordle Demo 1.0 (Alpha)
             </h2>
             <br />
             <form>
               <label htmlFor="id" style={{ "fontSize": "20px" }}>Name</label><br />
               <input type="text" id="name" name="name" onChange={handleNameChange} style={{ "fontSize": "20px" }} /><br /><br />
-              <div style={{"display" : "flex", "justifyContent" : "center"}} >
+              <div style={{ "display": "flex", "justifyContent": "center" }} >
                 <div>
                   <label htmlFor="id" style={{ "fontSize": "20px" }}>Room ID</label><br />
                   <input type="text" id="id" name="id" onChange={handleIdChange} style={{ "width": "70%", "fontSize": "20px" }} /><br />
@@ -159,7 +200,7 @@ function App() {
                 <h3>OR</h3>
                 <div>
                   <br />
-                  <input type="submit" onClick={handleCreate} value="Create" style={{ "fontSize": "20px", "marginLeft" : "30px"}} />
+                  <input type="submit" onClick={handleCreate} value="Create" style={{ "fontSize": "20px", "marginLeft": "30px" }} />
                 </div>
               </div>
             </form>
@@ -167,16 +208,27 @@ function App() {
           ) :
           (<div>
             <h2 style={{ "margin": "0px" }}>ROOM {roomId}</h2>
+            <h3>Players: {players.length}</h3>
+            <h3>{players.map(x => <sp style={{ "border": "solid 1px", "marginRight": "8px", "paddingLeft": "2px", "paddingRight": "2px" }}>{x[1]}</sp>)}</h3>
             <textarea readOnly value={messages.join("\n")} style={{ "overflowY": "scroll", "width": "500px", "height": "300px", "resize": "none", "fontSize": "18px" }}>
             </textarea>
             <br /><br />
             {isReady || (<button onClick={() => setIsReady(true)} style={{ "fontSize": "20px" }}>Ready</button>)}
             {isReady && (
-              <form onSubmit={handleGuess}>
-              <label htmlFor="guess" style={{ "fontSize": "20px" }}>Make a guess:</label><br />
-              <input type="text" id="fname" name="fname" onChange={handleGuessChange} style={{ "fontSize": "20px" }} /><br /><br /><br />
-              <input type="submit" value="Submit" style={{ "fontSize": "20px" }} />
-              </form>)}
+              gameStarted ? (
+                roundStarted ? (
+                  guessEnabled ? (
+                  <form onSubmit={handleGuess}>
+                    <label htmlFor="guess" style={{ "fontSize": "20px" }}>Make a guess:</label><br />
+                    <input type="text" id="fname" name="fname" onChange={handleGuessChange} style={{ "fontSize": "20px" }} /><br /><br /><br />
+                    <input type="submit" value="Submit" style={{ "fontSize": "20px" }} />
+                    </form>) :
+                    (<h3>No more guesses :(</h3>)
+                  ) : (
+                    gameOver ? <h3>Game ended.</h3> : <h3>Round starting soon...</h3>
+                )
+              ) : (<h3>Waiting for others to get READY...</h3>)
+            )}
           </div>
           )
         }
